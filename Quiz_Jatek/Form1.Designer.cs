e3d7d168c5b9065e3babf237baccd65cd0e28572
Bácskai Balázs <73953136+bacskaibalazs@users.noreply.github.com>
2021-05-26 16:30:00 +0200

    Add project files.

---
+++ Quiz_Jatek/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Quiz_Jatek
+{
+    partial class Quiz
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.AccessibleName = "";
+            this.pictureBox1.Image = global::Quiz_Jatek.Properties.Resources.Budapest;
+            this.pictureBox1.Location = new System.Drawing.Point(133, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(562, 288);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AccessibleName = "lblQuestion";
+            this.lblQuestion.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblQuestion.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblQuestion.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblQuestion.Location = new System.Drawing.Point(45, 335);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(715, 56);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Tag = "lblQuestion";
+            this.lblQuestion.Text = "Kérdés";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(246, 65);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.CheckAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(514, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(246, 65);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.CheckAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(45, 547);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(246, 65);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.CheckAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(514, 547);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(246, 65);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.CheckAnswerEvent);
+            // 
+            // Quiz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BackgroundImage = global::Quiz_Jatek.Properties.Resources.who_wants_to_be_a_millionaire;
+            this.ClientSize = new System.Drawing.Size(781, 649);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Quiz";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Quiz_Jatek/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_Jatek
+{
+    public partial class Quiz : Form
+    {
+        // Variables
+
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestion;
+
+
+
+
+
+
+        public Quiz()
+        {
+            InitializeComponent();
+
+            askQuestion(questionNumber);
+
+            totalQuestion = 8;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CheckAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if(buttonTag == correctAnswer)
+            {
+                score++;
+            }
+
+            if(questionNumber == totalQuestion)
+            {
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestion);
+
+
+                MessageBox.Show(
+                    "Quiz játék vége!" + Environment.NewLine + 
+                    "Volt " + score + " helyes válaszod" + Environment.NewLine +
+                    "A százalékod : " + percentage + "%" + Environment.NewLine +
+                    "Kattints az OK-ra az újra kezdéshez"
+                    );
+
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+
+            }
+
+            questionNumber++;
+            askQuestion(questionNumber);
+        }
+
+        private void askQuestion(int qnum)
+        {
+
+            switch(qnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.Budapest;
+
+                    lblQuestion.Text = "Melyik város látható a képen?";
+
+                    button1.Text = "Párizs";
+                    button2.Text = "Budapest";
+                    button3.Text = "Barcelona";
+                    button4.Text = "Kapuvár";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 2:
+                    pictureBox1.Image = Properties.Resources.Pápa;
+
+                    lblQuestion.Text = "Melyik pápa látható a képen?";
+
+                    button1.Text = "Ferenc pápa";
+                    button2.Text = "János pápa";
+                    button3.Text = "Benedek pápa";
+                    button4.Text = "Celesztin pápa";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 3:
+                    pictureBox1.Image = Properties.Resources.Badacsony;
+
+                    lblQuestion.Text = "Melyik hegy látható a képen?";
+
+                    button1.Text = "Badacsony";
+                    button2.Text = "Mátra";
+                    button3.Text = "Kékes";
+                    button4.Text = "Pilis";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 4:
+                    pictureBox1.Image = Properties.Resources.Hofi;
+
+                    lblQuestion.Text = "Mikor született a képen látható Hofi Géza?";
+
+                    button1.Text = "1953";
+                    button2.Text = "1965";
+                    button3.Text = "1944";
+                    button4.Text = "1936";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 5:
+                    pictureBox1.Image = Properties.Resources.HeimPal;
+
+                    lblQuestion.Text = "Melyik kerületben található a Heim Pál kórház?";
+
+                    button1.Text = "VIII.";
+                    button2.Text = "IX.";
+                    button3.Text = "XIII.";
+                    button4.Text = "VII.";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 6:
+                    pictureBox1.Image = Properties.Resources.Bíró_László_József;
+
+                    lblQuestion.Text = "Mit talált fel Bíró László József?";
+
+                    button1.Text = "Gyufa";
+                    button2.Text = "rubikkocka";
+                    button3.Text = "Dinamo";
+                    button4.Text = "golyóstoll";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 7:
+                    pictureBox1.Image = Properties.Resources.MezgaGeza;
+
+                    lblQuestion.Text = "Ki nem szerepel a Mézga Géza című rajzfilmsorozatban?";
+
+                    button1.Text = "Blöki";
+                    button2.Text = "Aladár";
+                    button3.Text = "MZ/X";
+                    button4.Text = "Nielsen";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 8:
+                    pictureBox1.Image = Properties.Resources.FeketeIstvan;
+
+                    lblQuestion.Text = "Melyik nem Fekete István regénye?";
+
+                    button1.Text = "Zsellérek";
+                    button2.Text = "A Pál utcai fiúk";
+                    button3.Text = "Hajnal Badányban";
+                    button4.Text = "Vuk";
+                    
+                    correctAnswer = 2;
+
+                    break;
+
+
+            }
+
+
+        }
+    }
+}
